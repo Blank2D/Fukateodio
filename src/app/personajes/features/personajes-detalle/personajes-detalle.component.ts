eba4e4ab868293b5522d58d2a306a6d21094667f
Cristobal <109001630+Blank2D@users.noreply.github.com>
2024-07-17 09:35:15 -0400

    PopUpAnimation

--- src/app/personajes/features/personajes-detalle/personajes-detalle.component.ts
+++ src/app/personajes/features/personajes-detalle/personajes-detalle.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { PersonajeDetailStateService } from '../../data-access/personaje-detail-state.service';
@@ -13,11 +13,39 @@ import { PersonajeDetailStateService } from '../../data-access/personaje-detail-
 })
 export default class PersonajesDetalleComponent implements OnInit {
   detallesVisible: boolean = false;
-
+  followers: string;
+  likes: string;
+  posts: string;
   personajeDetailState = inject(PersonajeDetailStateService).state;
 
   private route = inject(ActivatedRoute);
 
+  constructor(private cdr: ChangeDetectorRef) {
+    this.followers = this.generateRandomNumber(2000000);
+    this.likes = this.generateRandomNumber(2000000);
+    this.posts = this.generateRandomNumber(500); // Limitar posts a un máximo de 500
+  }
+
+  getRandomNumber(): number {
+    return Math.floor(Math.random() * 30) + 1;
+  }
+
+  generateRandomNumber(max: number): string {
+    const num = Math.floor(Math.random() * max) + 1;
+    return this.abbreviateNumber(num);
+  }
+
+  abbreviateNumber(value: number): string {
+    if (value < 1000) {
+      return value.toString();
+    } else if (value < 10000) {
+      return (value / 1000).toFixed(1) + 'K';
+    } else if (value < 1000000) {
+      return Math.floor(value / 1000) + 'K';
+    } else {
+      return (value / 1000000).toFixed(1) + 'M';
+    }
+  }
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       const id = params.get('id');
