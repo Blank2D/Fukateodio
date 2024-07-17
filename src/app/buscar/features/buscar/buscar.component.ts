1ab3e55e8eac3131bdf8e52f31e29451da8c3c92
Cristobal <109001630+Blank2D@users.noreply.github.com>
2024-07-17 07:05:56 -0400

    Buscar Avanzado parte 2

--- src/app/buscar/features/buscar/buscar.component.ts
+++ src/app/buscar/features/buscar/buscar.component.ts
@@ -1,24 +1,48 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal, computed } from '@angular/core';
 import { PersonajeStateService } from '../../../personajes/data-access/personajes-state.service';
 import { PersonajeCardComponent } from '../../../personajes/ui/personaje-card/personaje-card.component';
 
 @Component({
   selector: 'app-buscar',
   standalone: true,
-  imports: [PersonajeCardComponent, ],
+  imports: [PersonajeCardComponent],
   templateUrl: './buscar.component.html',
-  styleUrl: './buscar.component.scss',
+  styleUrls: ['./buscar.component.scss'],
   providers: [PersonajeStateService],
-
 })
-export default class BuscarComponent {
+export default class BuscarComponent implements OnInit {
   personajeState = inject(PersonajeStateService);
-  
-  page : number = 1;
+  page: number = 0;
+  searchTerm = signal<string>('');
+  personajes = computed(() => this.personajeState.state().personajes);
+  filteredPersonajes = computed(() => {
+    const term = this.searchTerm().toLowerCase();
+    if (term) {
+      return this.personajes().filter(personaje =>
+        personaje.name.toLowerCase().includes(term)
+      );
+    } else {
+      return this.personajes().slice(0, 200);
+    }
+  });
 
-  changePage() {
-    this.page = this.page + 1
-    this.personajeState.changePage$.next(this.page);
-    console.log(this.page);
+  async ngOnInit() {
+    // Ejecutar el método changePage 41 veces para cargar todos los personajes
+    for (let i = 1; i <= 42; i++) {
+      await this.changePage();
+    }
+  }
+
+  changePage(): Promise<void> {
+    return new Promise((resolve) => {
+      this.page = this.page + 1;
+      this.personajeState.changePage$.next(this.page);
+      console.log(this.page);
+      setTimeout(() => resolve(), 100); // Espera 100ms antes de resolver para asegurar que la llamada se procese
+    });
+  }
+
+  onSearchTermChange(event: any) {
+    this.searchTerm.set(event.target.value);
   }
 }
--- src/app/personajes/ui/personaje-card/personaje-card.component.ts
+++ src/app/personajes/ui/personaje-card/personaje-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectorRef, Component, input } from '@angular/core';
 import { Result, Root } from '../../../shared/interfaces/personaje.interface';
 import { RouterLink } from '@angular/router';
 
@@ -7,8 +7,43 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './personaje-card.component.html',
-  styleUrl: './personaje-card.component.scss'
+  styleUrl: './personaje-card.component.scss',
 })
 export class PersonajeCardComponent {
   personaje = input.required<Result>();
+
+  getRandomNumber(): number {
+    return Math.floor(Math.random() * 30) + 1;
+  }
+
+  followers: string;
+  likes: string;
+  posts: string;
+
+  constructor(private cdr: ChangeDetectorRef) {
+    this.followers = this.generateRandomNumber(2000000);
+    this.likes = this.generateRandomNumber(2000000);
+    this.posts = this.generateRandomNumber(500); // Limitar posts a un máximo de 500
+  }
+
+  ngOnInit(): void {
+    this.cdr.detectChanges();
+  }
+
+  generateRandomNumber(max: number): string {
+    const num = Math.floor(Math.random() * max) + 1;
+    return this.abbreviateNumber(num);
+  }
+
+  abbreviateNumber(value: number): string {
+    if (value < 1000) {
+      return value.toString();
+    } else if (value < 10000) {
+      return (value / 1000).toFixed(1) + 'K';
+    } else if (value < 1000000) {
+      return Math.floor(value / 1000) + 'K';
+    } else {
+      return (value / 1000000).toFixed(1) + 'M';
+    }
+  }
 }
